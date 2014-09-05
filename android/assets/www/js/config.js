ec15c90737d2cb821dc3f608b1876fe46016b8a3
Jonas Breme <jonas.breme@ixds.com>
2014-09-05 18:31:06 +0200

    Changed coordinated, dicided into tour1 and tour2.

--- android/assets/www/js/config.js
+++ android/assets/www/js/config.js
@@ -12,18 +12,19 @@ var HWPORT = 50000;
 // Tour places (lat, lon, path to mp3, artist, title, comment)
 
 var TOURS = {
-  "default": [
-    [52.54133, 13.403462, SDPATH + 'jeansteam.mp3',      'Jeans Team',                 'Lolita Dröhn',         SDPATH + 'jeansteam.png'],
-    [52.54522600, 13.41231100, SDPATH + 'nylon.mp3',          'Nylon',                      'Schönhauser Allee',    SDPATH + 'nylon.png'],
-    [52.53887000, 13.42433800, SDPATH + 'dra-q.mp3',          'Dra-Q',                      'Mitsommernacht',       SDPATH + 'dra-q.png'],
-    [52.53977900, 13.43593300, SDPATH + 'berlinskibeat.mp3',  'Berlinskibeat',              'Nacht in Berlin',      SDPATH + 'berlinskibeat.png'],
-    [52.53667600, 13.43234100, SDPATH + 'tonikater.mp3',      'Toni Kater',                 'Fuchslied',            SDPATH + 'tonikater.png'],
-    [52.52207000, 13.41280500, SDPATH + 'tresen.mp3',         'Der Singende Tresen',        'Alexanderplatz',       SDPATH + 'tresen.png'],
+  "tour1": [
+    [52.541898, 13.403592, SDPATH + 'jeansteam.mp3',      'Jeans Team',                 'Lolita Dröhn',         SDPATH + 'jeansteam.png'],
+    [52.545226, 13.412311, SDPATH + 'nylon.mp3',          'Nylon',                      'Schönhauser Allee',    SDPATH + 'nylon.png'],
+    [52.538870, 13.424338, SDPATH + 'dra-q.mp3',          'Dra-Q',                      'Mitsommernacht',       SDPATH + 'dra-q.png'],
+    [52.539575, 13.436428, SDPATH + 'berlinskibeat.mp3',  'Berlinskibeat',              'Nacht in Berlin',      SDPATH + 'berlinskibeat.png'],
+    [52.536676, 13.432341, SDPATH + 'tonikater.mp3',      'Toni Kater',                 'Fuchslied',            SDPATH + 'tonikater.png'],
+    [52.521166, 13.413264, SDPATH + 'tresen.mp3',         'Der Singende Tresen',        'Alexanderplatz',       SDPATH + 'tresen.png'],
     [52.511632, 13.426854, SDPATH + 'doering.mp3',        'Dirty Doering',              'Bye Bye Bar 25',       SDPATH + 'doering.png'],
-    [52.50865300, 13.45087300, SDPATH + 'koletzki.mp3',       'Oliver Koletzki feat. Fran', 'Warschauer Strasse',   SDPATH + 'koletzki.png'],
-    [52.50723000, 13.45590500, SDPATH + 'panda.mp3',          'Panda',                      'Hier bleiben',         SDPATH + 'panda.png'],
+    [52.508653, 13.450873, SDPATH + 'koletzki.mp3',       'Oliver Koletzki feat. Fran', 'Warschauer Strasse',   SDPATH + 'koletzki.png'],
+    [52.507230, 13.455905, SDPATH + 'panda.mp3',          'Panda',                      'Hier bleiben',         SDPATH + 'panda.png'],
+],
+"tour2": [
     [52.50839200, 13.42892100, SDPATH + 'rummelsnuff.mp3',    'Rummelsnuff',                'Winterlied',           SDPATH + 'rummelsnuff.png'],
-    //[52.51155900, 13.42684300, SDPATH + 'bonaparte.mp3',      'Bonaparte',                  'Gigolo Vagabundo',     SDPATH + 'bonaparte.png'],
     [52.49927600, 13.42049700, SDPATH + 'pitchtuner.mp3',     'Pitchtuner',                 'Unity Wins',           SDPATH + 'pitchtuner.png'],
     [52.49827100, 13.42440000, SDPATH + 'simonnejones.mp3',   'Simonne Jones',              'We Are Young',         SDPATH + 'simonnejones.png'],
     [52.49550000, 13.42014300, SDPATH + 'yukazu.mp3',         'Yukazu',                     'Rote Äpfel',           SDPATH + 'yukazu.png'],
